8f5f51b7caa1ad38d01c7d48196714d1b770a03e
Jesper Skov <jb1811@jyskebank.dk>
2020-10-21 09:52:36 +0200

    test single

--- src/test/java/org/acme/quickstart/RestClientTest.java
+++ src/test/java/org/acme/quickstart/RestClientTest.java
@@ -1,11 +1,19 @@
 package org.acme.quickstart;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
 import javax.inject.Inject;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
 /**
@@ -16,12 +24,35 @@ class RestClientTest {
 	@Inject @RestClient HelloApi helloService;
 	
 	@Test
-	void canEcho() {
+	void canConsumeSingle() throws InterruptedException {
 		Uni<String> response = helloService.greeting("foobar");
+
+		CountDownLatch done = new CountDownLatch(1);
+		AtomicReference<String> item = new AtomicReference<>();
+
+		response
+			.subscribe()
+			.with(s -> {
+				item.set(s);
+				done.countDown();;
+			});
 		
-		System.out.println("Got " + response);
-		response.subscribe()
-			.with(System.out::println);
+		if (!done.await(5, TimeUnit.SECONDS)) {
+			fail("Failed to receive data in time");
+		}
+		
+		assertEquals("hello foobar", item.get());
+	}
+
+//	@Test
+	void canConsumeList() {
+		Multi<String> response = helloService.greetings(7, "foobar");
+		
+		response
+			.subscribe()
+			.with(
+					msg -> System.out.println("Message " + msg),
+					failure -> fail("List consumtion failed", failure));
 	}
 
 }
